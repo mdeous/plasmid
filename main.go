af8bd8d753899579e264f8b2fc39a8593b7b4e5e
Mathieu Deous <mat.deous@gmail.com>
2022-07-08 16:48:03 +0200

    working poc

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/x509"
-	"encoding/pem"
+	"crypto/x509/pkix"
+	"encoding/xml"
+	"fmt"
+	"math/big"
 	"net/url"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/zenazn/goji"
 	"golang.org/x/crypto/bcrypt"
@@ -13,103 +20,117 @@ import (
 	"github.com/crewjam/saml/samlidp"
 )
 
-var key = func() crypto.PrivateKey {
-	b, _ := pem.Decode([]byte(`-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEA0OhbMuizgtbFOfwbK7aURuXhZx6VRuAs3nNibiuifwCGz6u9
-yy7bOR0P+zqN0YkjxaokqFgra7rXKCdeABmoLqCC0U+cGmLNwPOOA0PaD5q5xKhQ
-4Me3rt/R9C4Ca6k3/OnkxnKwnogcsmdgs2l8liT3qVHP04Oc7Uymq2v09bGb6nPu
-fOrkXS9F6mSClxHG/q59AGOWsXK1xzIRV1eu8W2SNdyeFVU1JHiQe444xLoPul5t
-InWasKayFsPlJfWNc8EoU8COjNhfo/GovFTHVjh9oUR/gwEFVwifIHihRE0Hazn2
-EQSLaOr2LM0TsRsQroFjmwSGgI+X2bfbMTqWOQIDAQABAoIBAFWZwDTeESBdrLcT
-zHZe++cJLxE4AObn2LrWANEv5AeySYsyzjRBYObIN9IzrgTb8uJ900N/zVr5VkxH
-xUa5PKbOcowd2NMfBTw5EEnaNbILLm+coHdanrNzVu59I9TFpAFoPavrNt/e2hNo
-NMGPSdOkFi81LLl4xoadz/WR6O/7N2famM+0u7C2uBe+TrVwHyuqboYoidJDhO8M
-w4WlY9QgAUhkPyzZqrl+VfF1aDTGVf4LJgaVevfFCas8Ws6DQX5q4QdIoV6/0vXi
-B1M+aTnWjHuiIzjBMWhcYW2+I5zfwNWRXaxdlrYXRukGSdnyO+DH/FhHePJgmlkj
-NInADDkCgYEA6MEQFOFSCc/ELXYWgStsrtIlJUcsLdLBsy1ocyQa2lkVUw58TouW
-RciE6TjW9rp31pfQUnO2l6zOUC6LT9Jvlb9PSsyW+rvjtKB5PjJI6W0hjX41wEO6
-fshFELMJd9W+Ezao2AsP2hZJ8McCF8no9e00+G4xTAyxHsNI2AFTCQcCgYEA5cWZ
-JwNb4t7YeEajPt9xuYNUOQpjvQn1aGOV7KcwTx5ELP/Hzi723BxHs7GSdrLkkDmi
-Gpb+mfL4wxCt0fK0i8GFQsRn5eusyq9hLqP/bmjpHoXe/1uajFbE1fZQR+2LX05N
-3ATlKaH2hdfCJedFa4wf43+cl6Yhp6ZA0Yet1r8CgYEAwiu1j8W9G+RRA5/8/DtO
-yrUTOfsbFws4fpLGDTA0mq0whf6Soy/96C90+d9qLaC3srUpnG9eB0CpSOjbXXbv
-kdxseLkexwOR3bD2FHX8r4dUM2bzznZyEaxfOaQypN8SV5ME3l60Fbr8ajqLO288
-wlTmGM5Mn+YCqOg/T7wjGmcCgYBpzNfdl/VafOROVbBbhgXWtzsz3K3aYNiIjbp+
-MunStIwN8GUvcn6nEbqOaoiXcX4/TtpuxfJMLw4OvAJdtxUdeSmEee2heCijV6g3
-ErrOOy6EqH3rNWHvlxChuP50cFQJuYOueO6QggyCyruSOnDDuc0BM0SGq6+5g5s7
-H++S/wKBgQDIkqBtFr9UEf8d6JpkxS0RXDlhSMjkXmkQeKGFzdoJcYVFIwq8jTNB
-nJrVIGs3GcBkqGic+i7rTO1YPkquv4dUuiIn+vKZVoO6b54f+oPBXd4S0BnuEqFE
-rdKNuCZhiaE2XD9L/O9KP1fh5bfEcKwazQ23EvpJHBMm8BGC+/YZNw==
------END RSA PRIVATE KEY-----`))
-	k, _ := x509.ParsePKCS1PrivateKey(b.Bytes)
-	return k
-}()
+type IdpKeys struct {
+	Certificate *x509.Certificate
+	PrivateKey  *rsa.PrivateKey
+}
 
-var cert = func() *x509.Certificate {
-	b, _ := pem.Decode([]byte(`-----BEGIN CERTIFICATE-----
-MIIDBzCCAe+gAwIBAgIJAPr/Mrlc8EGhMA0GCSqGSIb3DQEBBQUAMBoxGDAWBgNV
-BAMMD3d3dy5leGFtcGxlLmNvbTAeFw0xNTEyMjgxOTE5NDVaFw0yNTEyMjUxOTE5
-NDVaMBoxGDAWBgNVBAMMD3d3dy5leGFtcGxlLmNvbTCCASIwDQYJKoZIhvcNAQEB
-BQADggEPADCCAQoCggEBANDoWzLos4LWxTn8Gyu2lEbl4WcelUbgLN5zYm4ron8A
-hs+rvcsu2zkdD/s6jdGJI8WqJKhYK2u61ygnXgAZqC6ggtFPnBpizcDzjgND2g+a
-ucSoUODHt67f0fQuAmupN/zp5MZysJ6IHLJnYLNpfJYk96lRz9ODnO1Mpqtr9PWx
-m+pz7nzq5F0vRepkgpcRxv6ufQBjlrFytccyEVdXrvFtkjXcnhVVNSR4kHuOOMS6
-D7pebSJ1mrCmshbD5SX1jXPBKFPAjozYX6PxqLxUx1Y4faFEf4MBBVcInyB4oURN
-B2s59hEEi2jq9izNE7EbEK6BY5sEhoCPl9m32zE6ljkCAwEAAaNQME4wHQYDVR0O
-BBYEFB9ZklC1Ork2zl56zg08ei7ss/+iMB8GA1UdIwQYMBaAFB9ZklC1Ork2zl56
-zg08ei7ss/+iMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADggEBAAVoTSQ5
-pAirw8OR9FZ1bRSuTDhY9uxzl/OL7lUmsv2cMNeCB3BRZqm3mFt+cwN8GsH6f3uv
-NONIhgFpTGN5LEcXQz89zJEzB+qaHqmbFpHQl/sx2B8ezNgT/882H2IH00dXESEf
-y/+1gHg2pxjGnhRBN6el/gSaDiySIMKbilDrffuvxiCfbpPN0NRRiPJhd2ay9KuL
-/RxQRl1gl9cHaWiouWWba1bSBb2ZPhv2rPMUsFo98ntkGCObDX6Y1SpkqmoTbrsb
-GFsTG2DLxnvr4GdN1BSr0Uu/KV3adj47WkXVPeMYQti/bQmxQB8tRFhrw80qakTL
-UzreO96WzlBBMtY=
------END CERTIFICATE-----`))
-	c, _ := x509.ParseCertificate(b.Bytes)
-	return c
-}()
+func getEnv(varName string, defaultVal string) string {
+	value, exists := os.LookupEnv(varName)
+	if !exists {
+		return defaultVal
+	}
+	return value
+}
+
+func generateKeys() (*IdpKeys, error) {
+	// generate CA
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject: pkix.Name{
+			Organization:  []string{getEnv("IDP_CA_NAME", "Example Org")},
+			Country:       []string{getEnv("IDP_CA_COUNTRY", "FR")},
+			Province:      []string{getEnv("IDP_CA_PROVINCE", "")},
+			Locality:      []string{getEnv("IDP_CA_LOCALITY", "Paris")},
+			StreetAddress: []string{getEnv("IDP_CA_ADDRESS", "")},
+			PostalCode:    []string{getEnv("IDP_CA_POSTCODE", "")},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	// generate private key
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, fmt.Errorf("unable to generate private key: %v", err)
+	}
+
+	// generate certificate
+	certBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &privKey.PublicKey, privKey)
+	if err != nil {
+		return nil, fmt.Errorf("unable to generate certificate: %v", err)
+	}
+	cert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse certificate: %v", err)
+	}
+
+	// build keys struct
+	keys := &IdpKeys{
+		Certificate: cert,
+		PrivateKey:  privKey,
+	}
+	return keys, nil
+}
 
 func main() {
 	logr := logger.DefaultLogger
-	baseURL, _ := url.Parse("/")
 
+	logr.Println("generating identity provider keys")
+	keys, err := generateKeys()
+	if err != nil {
+		logr.Fatalln(err.Error())
+	}
+
+	logr.Println("preparing identity provider")
+	baseUrl, _ := url.Parse("http://127.0.0.1:8000")
 	idpServer, err := samlidp.New(samlidp.Options{
-		URL:         *baseURL,
-		Key:         key,
+		URL:         *baseUrl,
+		Key:         keys.PrivateKey,
 		Logger:      logr,
-		Certificate: cert,
+		Certificate: keys.Certificate,
 		Store:       &samlidp.MemoryStore{},
 	})
 	if err != nil {
-		logr.Fatalf("%s", err)
+		logr.Fatalf(err.Error())
 	}
+	metaXml, err := xml.MarshalIndent(idpServer.IDP.Metadata(), "", "    ")
+	if err != nil {
+		logr.Fatalf("unable to marshal metadata: %v", err)
+	}
+	logr.Printf("identity provider metadata:\n%s", metaXml)
 
-	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("hunter2"), bcrypt.DefaultCost)
-	err = idpServer.Store.Put("/users/alice", samlidp.User{Name: "alice",
+	username := getEnv("IDP_USER_NAME", "admin")
+	password := getEnv("IDP_USER_PASSWORD", "Password123")
+	groups := strings.Split(getEnv("IDP_USER_GROUPS", "Administrators,Users"), ",")
+	fullName := getEnv("IDP_USER_FULLNAME", "Admin User")
+	givenName := getEnv("IDP_USER_GIVENNAME", "Admin")
+	surname := getEnv("IDP_USER_SURNAME", "User")
+	email := getEnv("IDP_USER_EMAIL", "admin@example.com")
+
+	logr.Printf("creating new user: %s", username)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		logr.Fatalf("unable to hash user password: %v", err)
+	}
+	err = idpServer.Store.Put("/users/"+username, samlidp.User{
+		Name:           username,
 		HashedPassword: hashedPassword,
-		Groups:         []string{"Administrators", "Users"},
-		Email:          "alice@example.com",
-		CommonName:     "Alice Smith",
-		Surname:        "Smith",
-		GivenName:      "Alice",
+		Groups:         groups,
+		Email:          email,
+		CommonName:     fullName,
+		Surname:        surname,
+		GivenName:      givenName,
 	})
 	if err != nil {
-		logr.Fatalf("%s", err)
+		logr.Fatalf(err.Error())
 	}
 
-	err = idpServer.Store.Put("/users/bob", samlidp.User{
-		Name:           "bob",
-		HashedPassword: hashedPassword,
-		Groups:         []string{"Users"},
-		Email:          "bob@example.com",
-		CommonName:     "Bob Smith",
-		Surname:        "Smith",
-		GivenName:      "Bob",
-	})
-	if err != nil {
-		logr.Fatalf("%s", err)
-	}
-
+	logr.Println("starting identity provider server")
 	goji.Handle("/*", idpServer)
 	goji.Serve()
 }
