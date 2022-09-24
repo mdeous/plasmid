93576983114873c7ea528d37a69f603e644aad1c
Mathieu Deous <mat.deous@gmail.com>
2022-09-24 16:00:26 +0200

    add services client API

---
+++ pkg/client/services.go
@@ -0,0 +1,58 @@
+package client
+
+import (
+	"fmt"
+	idp "github.com/crewjam/saml/samlidp"
+	"net/http"
+)
+
+type serviceIds struct {
+	Services []string `json:"services"`
+}
+
+type ServiceList struct {
+	Services []*idp.Service
+}
+
+func (p *PlasmidClient) ServiceAdd(service *idp.Service) error {
+	err := p.resourceAdd("services", service.Name, service)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *PlasmidClient) ServiceList() ([]string, error) {
+	ids := &serviceIds{}
+	err := p.resourceIds("services", ids)
+	if err != nil {
+		return nil, err
+	}
+	return ids.Services, nil
+}
+
+func (p *PlasmidClient) ServiceDel(serviceName string) error {
+	// get list of serviceNames
+	ids := &serviceIds{}
+	err := p.resourceIds("services", ids)
+	if err != nil {
+		return err
+	}
+	// check if service exists
+	serviceExists := false
+	for _, existingName := range ids.Services {
+		if existingName == serviceName {
+			serviceExists = true
+			break
+		}
+	}
+	if !serviceExists {
+		return fmt.Errorf("service not found: %s", serviceName)
+	}
+	// delete service
+	_, _, err = p.request(http.MethodDelete, "/services/"+serviceName, nil, http.StatusNoContent)
+	if err != nil {
+		return err
+	}
+	return nil
+}
