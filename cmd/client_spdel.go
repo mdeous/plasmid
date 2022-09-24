33cd2a0694dad0da3886e86cfcf3605f3030a020
Mathieu Deous <mat.deous@gmail.com>
2022-09-24 16:08:31 +0200

    add sp-del command

---
+++ cmd/client_spdel.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/mdeous/plasmid/pkg/client"
+	"github.com/mdeous/plasmid/pkg/config"
+	"github.com/spf13/viper"
+
+	"github.com/spf13/cobra"
+)
+
+// spdelCmd represents the spGet command
+var spdelCmd = &cobra.Command{
+	Use:   "sp-del",
+	Short: "Delete a service provider",
+	Run: func(cmd *cobra.Command, args []string) {
+		// get target service from command line
+		svc, err := cmd.Flags().GetString("service")
+		// create plasmid client
+		c, err := client.New(viper.GetString(config.BaseUrl))
+		if err != nil {
+			logr.Fatalf(err.Error())
+		}
+		// delete service
+		err = c.ServiceDel(svc)
+		if err != nil {
+			logr.Fatalf(err.Error())
+		}
+	},
+}
+
+func init() {
+	clientCmd.AddCommand(spdelCmd)
+	spdelCmd.Flags().StringP("service", "s", "", "id of service provider to delete")
+}
