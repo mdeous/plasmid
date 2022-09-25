dfb207d5d2d786d7177a0386cda004381d6b7fde
Mathieu Deous <mat.deous@gmail.com>
2022-09-25 12:12:19 +0200

    add flag to display build info with version

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
@@ -14,10 +15,21 @@ var versionCmd = &cobra.Command{
 	Aliases: []string{"ver", "v"},
 	Short:   "Show program version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		verbose, err := cmd.Flags().GetBool("verbose")
+		if err != nil {
+			logr.Fatalf(err.Error())
+		}
+		versionInfo := ""
+		if verbose {
+			versionInfo = fmt.Sprintf("%s (built with %s for %s-%s)", version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+		} else {
+			versionInfo = version
+		}
+		fmt.Println(versionInfo)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	versionCmd.Flags().BoolP("verbose", "v", false, "display build information")
 }
